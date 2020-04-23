080d465ac9537a14260656c6e3441057d8091dce
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-04-23 19:31:51 +0530

    background improvement

--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -2,17 +2,22 @@ import React from "react";
 
 function FaceDetection({ url }) {
   return (
-    <div
-      className="center"
-      style={{
-        marginTop: "20px",
-
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <img alt="" style={{ width: "400px", height: "250px" }} src={url} />
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img alt="" src={url} width="500px" height="auto"></img>
+      </div>
     </div>
+    // <div
+    //   className="center"
+    //   style={{
+    //     marginTop: "20px",
+
+    //     display: "flex",
+    //     justifyContent: "center",
+    //   }}
+    // >
+    //   <img alt="" style={{ width: "400px", height: "250px" }} src={url} />
+    // </div>
   );
 }
 
