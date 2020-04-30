f5ed0bb6025f9fe4d08fa0d5edd6e7cf64d74c89
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-04-30 20:20:29 +0530

    express routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {});
+app.post("/signin", (req, res) => {});
+app.post("/register", (req, res) => {});
+app.post("/");
 app.listen(3000, () => {
   console.log("Server running ...");
 });
